2c09e9d7b4d93309ec803ff5bc41fe1ff6f55269
Frédéric Dehédin <frederic.dehedin@belsoft.ch>
2016-06-20 15:59:13 +0200

    implemented applicationcontroller and page title

---
+++ nsf/Code/Java/ch/belsoft/hrassistant/controller/ApplicationController.java
@@ -0,0 +1,13 @@
+package ch.belsoft.hrassistant.controller;
+
+import java.io.Serializable;
+
+import ch.belsoft.hrassistant.config.model.ConfigType;
+
+public abstract class ApplicationController implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
