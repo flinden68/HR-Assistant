5ce130fea8a9c8f00b8cd11e9587307297ef1e19
Frank van der Linden <flinden68@elstarit.nl>
2016-06-21 21:29:25 +0200

    Moved external jars to the correct location where a normal Java server, also Domino,  is expecting the jars. WebContent/WEB-INF/lib

--- nsf/Code/Java/ch/belsoft/hrassistant/controller/ApplicationController.java
+++ nsf/Code/Java/ch/belsoft/hrassistant/controller/ApplicationController.java
@@ -2,12 +2,14 @@ package ch.belsoft.hrassistant.controller;
 
 import java.io.Serializable;
 
-import ch.belsoft.hrassistant.config.model.ConfigType;
-
 public abstract class ApplicationController implements Serializable {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+    public ApplicationController(){}
+    
+}
 
