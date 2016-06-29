ba9f9c5a2d104ffb45af5df71e956384363befda
Frédéric Dehédin <frederic.dehedin@belsoft.ch>
2016-06-29 13:56:48 +0200

    fixed configurations

--- nsf/Code/Java/ch/belsoft/hrassistant/config/dao/ConfigurationDAO.java
+++ nsf/Code/Java/ch/belsoft/hrassistant/config/dao/ConfigurationDAO.java
@@ -13,50 +13,51 @@ public class ConfigurationDAO implements ICrudDAO<ConfigDefault, String>,
 	/**
      * 
      */
-    private static final long serialVersionUID = 1L;
-    private CloudantService cloudantService = null;
-    private static final String DESIGN_DOC = "configuration";
-    private static final String VIEW_NAME = "configurations";
-    
-    public void update(IConfiguration config) {
-        connectToService();
-        cloudantService.updateDocument(config);
-    }
-    
-    public void create(IConfiguration config) {
-        connectToService();
-        cloudantService.saveDocument(config);
-    }
-    
-    public void delete(IConfiguration config) {
-        connectToService();
-        cloudantService.removeDocument(config);
-    }
-    
-    public IConfiguration read(String id) {
-        connectToService();
-        return (IConfiguration) cloudantService.findDocumentByID(
-                IConfiguration.class, id);
-    }
-    
-    @SuppressWarnings("unchecked")
-    public List<IConfiguration> read() {
-        connectToService();
-        return (List<IConfiguration>) cloudantService
-        .findAllDocuments(IConfiguration.class);
-    }
-    
-    public void connectToService() {
-        if (!cloudantService.isConnected()) {
-            cloudantService.connect();
-        }
-    }
-    
-    public CloudantService getCloudantService() {
-        return cloudantService;
-    }
-    
-    public void setCloudantService(CloudantService cloudantService) {
-        this.cloudantService = cloudantService;
-    }
+	private static final long serialVersionUID = 1L;
+	private CloudantService cloudantService = null;
+	private static final String DESIGN_DOC = "configuration";
+	private static final String VIEW_NAME = "configurations";
+
+	public void update(ConfigDefault config) {
+		connectToService();
+		cloudantService.updateDocument(config);
+	}
+
+	public void create(ConfigDefault config) {
+		connectToService();
+		cloudantService.saveDocument(config);
+	}
+
+	public void delete(ConfigDefault config) {
+		connectToService();
+		cloudantService.removeDocument(config);
+	}
+
+	public ConfigDefault read(String id) {
+		connectToService();
+		return (ConfigDefault) cloudantService.findDocumentByID(
+				IConfiguration.class, id);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<ConfigDefault> read() {
+		connectToService();
+		return (List<ConfigDefault>) cloudantService
+				.findAllDocuments(ConfigDefault.class);
+	}
+
+	public void connectToService() {
+		if (!cloudantService.isConnected()) {
+			cloudantService.connect();
+		}
+	}
+
+	public CloudantService getCloudantService() {
+		return cloudantService;
+	}
+
+	public void setCloudantService(CloudantService cloudantService) {
+		this.cloudantService = cloudantService;
+	}
+
 }
