b3d57db92bf965346b016483c9454cee1beea19d
Frédéric Dehédin <frederic.dehedin@belsoft.ch>
2016-08-06 10:41:28 +0200

    aaaand the implementation of the factory

--- nsf/Code/Java/ch/belsoft/charts/factory/ChartFactoryProducer.java
+++ nsf/Code/Java/ch/belsoft/charts/factory/ChartFactoryProducer.java
@@ -28,4 +28,14 @@ public class ChartFactoryProducer implements Serializable {
 				ChartFactoryToneAnalyzer.ToneCategoryEnum.EMOTION_TONE);
 	}
 
+	public ChartFactoryToneAnalyzer getChartFactoryToneAnalyzerLanguageTone() {
+		return new ChartFactoryToneAnalyzer(
+				ChartFactoryToneAnalyzer.ToneCategoryEnum.LANGUAGE_TONE);
+	}
+
+	public ChartFactoryToneAnalyzer getChartFactoryToneAnalyzerSocialTone() {
+		return new ChartFactoryToneAnalyzer(
+				ChartFactoryToneAnalyzer.ToneCategoryEnum.SOCIAL_TONE);
+	}
+
 }
