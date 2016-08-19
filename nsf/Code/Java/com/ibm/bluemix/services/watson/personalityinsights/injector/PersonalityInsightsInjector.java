70e1b3617de3abfca1ca31b4457e68185b932f08
Frank van der Linden <flinden68@elstarit.nl>
2016-08-19 11:02:40 +0200

    Fixed a error in the PersonalityInsightsInjector by adding getters and setters Added more search capabilities at the job_listings page

--- nsf/Code/Java/com/ibm/bluemix/services/watson/personalityinsights/injector/PersonalityInsightsInjector.java
+++ nsf/Code/Java/com/ibm/bluemix/services/watson/personalityinsights/injector/PersonalityInsightsInjector.java
@@ -2,43 +2,54 @@ package com.ibm.bluemix.services.watson.personalityinsights.injector;
 
 import java.io.Serializable;
 
-import org.apache.commons.lang3.StringEscapeUtils;
-
-import com.ibm.bluemix.services.watson.toneanalyzer.interfaces.ToneAnalyzable;
-import com.ibm.bluemix.services.watson.toneanalyzer.model.ToneAnalyzerRequest;
-import com.ibm.bluemix.services.watson.toneanalyzer.service.ToneAnalyzerConsumer;
-import com.ibm.bluemix.services.watson.toneanalyzer.service.ToneAnalyzerService;
-
-import ch.belsoft.tools.Logging;
 import ch.belsoft.tools.XPagesUtil;
 
-public class PersonalityInsightsInjector implements Serializable {
+import com.ibm.bluemix.services.watson.personalityinsights.service.PersonalityInsightsService;
+import com.ibm.bluemix.services.watson.toneanalyzer.service.ToneAnalyzerConsumer;
 
-	/**
+public class PersonalityInsightsInjector implements Serializable {
+    
+    /**
      * 
      */
-	private static final long serialVersionUID = 1L;
-
-	private PersonalityInsightsService personalityInsightsService = null;
-	private ToneAnalyzerConsumer toneAnalyzerConsumer = null;
-
-	private static final String BEAN_NAME = "personalityInsightsInjector";
-
-	public PersonalityInsightsInjector() {
-
-	}
-
-	// access to the bean
-	public static PersonalityInsightsInjector get() {
-		return (PersonalityInsightsInjector) XPagesUtil.resolveVariable(BEAN_NAME);
-	}
-
-	public ToneAnalyzerService getToneAnalyzerService() {
+    private static final long serialVersionUID = 1L;
+    
+    private PersonalityInsightsService personalityInsightsService = null;
+    private ToneAnalyzerConsumer toneAnalyzerConsumer = null;
+    
+    private static final String BEAN_NAME = "personalityInsightsInjector";
+    
+    public PersonalityInsightsInjector() {
+        
+    }
+    
+    public ToneAnalyzerConsumer getToneAnalyzerConsumer() {
+        return toneAnalyzerConsumer;
+    }
+    
+    public void setToneAnalyzerConsumer(ToneAnalyzerConsumer toneAnalyzerConsumer) {
+        this.toneAnalyzerConsumer = toneAnalyzerConsumer;
+    }
+    
+    public void setPersonalityInsightsService(PersonalityInsightsService personalityInsightsService) {
+        this.personalityInsightsService = personalityInsightsService;
+    }
+    
+    public PersonalityInsightsService getPersonalityInsightsService() {
+        return personalityInsightsService;
+    }
+    
+    // access to the bean
+    public static PersonalityInsightsInjector get() {
+        return (PersonalityInsightsInjector) XPagesUtil.resolveVariable(BEAN_NAME);
+    }
+    
+    /*public ToneAnalyzerService getToneAnalyzerService() {
 		return toneAnalyzerService;
 	}
 
 	public void setToneAnalyzerService(ToneAnalyzerService toneAnalyzerService) {
 		this.toneAnalyzerService = toneAnalyzerService;
-	}
-
+	}*/
+    
 }
