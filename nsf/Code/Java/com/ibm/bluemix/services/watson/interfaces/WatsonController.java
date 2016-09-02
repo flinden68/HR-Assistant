a6672cfa38186180fe90135b054a3f197e9dbbb9
Frédéric Dehédin <frederic.dehedin@belsoft.ch>
2016-09-02 10:53:59 +0200

    added loading indicator on menu items, implemented WatsonController Interface in order to let the classes implement the analyzeTextAllWatsonServices method, Watson Icon

---
+++ nsf/Code/Java/com/ibm/bluemix/services/watson/interfaces/WatsonController.java
@@ -0,0 +1,11 @@
+package com.ibm.bluemix.services.watson.interfaces;
+
+public abstract interface WatsonController {
+
+	/**
+	 * Analyses all watson services.. important: init the analyzable object
+	 * first!
+	 */
+	public void analyzeTextAllWatsonServices();
+
+}
